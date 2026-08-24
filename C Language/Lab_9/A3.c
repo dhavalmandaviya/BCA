7304f27c9cbfb4d14b9f678f3ceff3d1ca4e998c
Dhaval Mandaviya <119856276+dhavalmandaviya@users.noreply.github.com>
2026-08-24 21:40:35 +0530

    Create A3.c

---
+++ C Language/Lab_9/A3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    float f, c;
+
+    printf("Enter Fahrenheit : ");
+    scanf("%f", &f);
+
+    c = (((f - 32) * 5)) / 9;
+
+    printf("Celsius = %f", c);
+}
