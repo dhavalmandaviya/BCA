648107db5378f2a39bc5227d9dde7538f2984e40
Dhaval Mandaviya <119856276+dhavalmandaviya@users.noreply.github.com>
2026-08-24 22:03:01 +0530

    Create A4.c

---
+++ C Language/Lab_11/A4.c
@@ -0,0 +1,24 @@
+/* LAB - 11
+   4. Find largest number from given three numbers
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, largest;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a >= b && a >= c)
+        largest = a;
+    else if (b >= a && b >= c)
+        largest = b;
+    else
+        largest = c;
+
+    printf("Largest number = %d", largest);
+
+    return 0;
+}
