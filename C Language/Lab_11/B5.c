388d448802648bacc22f304b257ffe7665a96ef9
Dhaval Mandaviya <119856276+dhavalmandaviya@users.noreply.github.com>
2026-08-24 22:03:40 +0530

    Create B5.c

---
+++ C Language/Lab_11/B5.c
@@ -0,0 +1,24 @@
+/* LAB - 11
+   5. Check whether character is vowel or consonant
+   Using single if only
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' ||
+        ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' ||
+        ch == 'O' || ch == 'U')
+        printf("Vowel");
+    else
+        printf("Consonant");
+
+    return 0;
+}
