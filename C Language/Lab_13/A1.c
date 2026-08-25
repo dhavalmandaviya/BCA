af8b483405442b5dc0650675e68bef7cd2275af1
Dhaval Mandaviya <119856276+dhavalmandaviya@users.noreply.github.com>
2026-08-25 21:28:51 +0530

    Implement weekday printer based on user input
    This program prompts the user to enter a day number and prints the corresponding weekday or an error message for invalid input.

---
+++ C Language/Lab_13/A1.c
@@ -0,0 +1,30 @@
+/* Lab_13
+   1. Print weekday based on given number.
+*/
+#include <stdio.h>
+
+int main() {
+    int day;
+    printf("Enter day number (1-7): \n");
+    scanf("%d", &day);
+
+    if (day == 1) {
+        printf("Monday\n");
+    } else if (day == 2) {
+        printf("Tuesday\n");
+    } else if (day == 3) {
+        printf("Wednesday\n");
+    } else if (day == 4) {
+        printf("Thursday\n");
+    } else if (day == 5) {
+        printf("Friday\n");
+    } else if (day == 6) {
+        printf("Saturday\n");
+    } else if (day == 7) {
+        printf("Sunday\n");
+    } else {
+        printf("Invalid input! Please enter a number between 1 and 7.\n");
+    }
+
+    return 0;
+}
