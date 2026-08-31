57b2bc9e932ad97289cf0dfb19f34c90ddd806a3
Dhaval Mandaviya <119856276+dhavalmandaviya@users.noreply.github.com>
2026-08-31 17:55:33 +0530

    Implement month days printer using switch statement
    This program prompts the user for a month number and prints the number of days in that month using a switch statement.

---
+++ C Language/Lab_14/A2.c
@@ -0,0 +1,39 @@
+/*
+2. Print number of days in a month using switch.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int month;
+
+    printf("Enter month number (1-12): ");
+    scanf("%d", &month);
+
+    switch(month)
+    {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            printf("January has 31 days");
+            break;
+        case 2:
+            printf("February has 28 days");
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            printf("April has 30 days");
+            break;
+        default:
+            printf("Invalid month number");
+    }
+
+    return 0;
+}
