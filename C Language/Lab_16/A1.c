d37fd0bee382c2847c7e6202ae41707eb38ace36
Dhaval Mandaviya <119856276+dhavalmandaviya@users.noreply.github.com>
2026-09-08 16:57:49 +0530

    Implement while loop to print numbers 1 to 10

---
+++ C Language/Lab_16/A1.c
@@ -0,0 +1,16 @@
+/* Question: Print 1 to 10 using While loop. */
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+
+    while(i <= 10)
+    {
+        printf("%d ", i);
+        i++;
+    }
+
+    return 0;
+}
