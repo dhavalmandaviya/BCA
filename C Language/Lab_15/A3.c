66b0fbb6498f47b4b327bf68bd259b318b188e05
Dhaval Mandaviya <119856276+dhavalmandaviya@users.noreply.github.com>
2026-09-08 16:53:37 +0530

    Create A3.c

---
+++ C Language/Lab_15/A3.c
@@ -0,0 +1,19 @@
+/*
+ 3. Find out largest number from given 3 numbers using conditional operator.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, largest;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    largest = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+
+    printf("Largest number = %d", largest);
+
+    return 0;
+}
